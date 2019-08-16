f3aa8923d724a1cc36e2b1d10703a850513f9093
timkeane <tkeane@doitt.nyc.gov>
2019-08-16 09:53:41 -0400

    added simple test

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -1,1 +1,11 @@
-//Nothing here
+import FinderApp from 'nyc-lib/nyc/ol/FinderApp'
+import App from '../src/js/App'
+
+test('constructor', () => {
+  expect.assertions(2)
+  
+  const app = new App()
+
+  expect(app instanceof App).toBe(true)
+  expect(app instanceof FinderApp).toBe(true)
+})
